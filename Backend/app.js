db8d57fb5b07c6ab9fb6e29f80a5e35a0c564c02
20230300@ricaldone.edu.sv <20230300@ricaldone.edu.sv>
2025-06-14 14:44:30 -0600

    encriptacion,Login y Logout creados

--- Backend/app.js
+++ Backend/app.js
@@ -3,13 +3,20 @@ import camionesRoutes from "./src/Routes/camionesRoutes.js"
 import empleadoRoutes from "./src/Routes/empleadosRoutes.js"
 import motoristasRoutes from "./src/Routes/motoristaRoutes.js"
 import proveedoresRoutes from "./src/Routes/proveedoresRoutes.js"
+import LoginRoutes from "./src/Routes/LoginRoutes.js"
+import cookieParser from "cookie-parser"
+import Logout from "./src/Routes/Logout.js"
 
 const app = express();
 app.use(express.json());
+app.use(cookieParser());
+
 
 app.use("/api/camiones",camionesRoutes);
 app.use("/api/empleados",empleadoRoutes);
 app.use("/api/motoristas",motoristasRoutes);
 app.use("/api/proveedores",proveedoresRoutes);
+app.use("/api/Login",LoginRoutes);
+app.use("/api/Logout",Logout);
 
 export default app;
