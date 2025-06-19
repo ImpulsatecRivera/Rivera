2f8ab499162805fa07fe67d833a4fceed9ba36fa
Angel <angelfan1637@gmail.com>
2025-06-19 08:13:32 -0600

    Corrigiendo el login y el input

--- Frontend/page-Private/src/components/Login/Input.jsx
+++ Frontend/page-Private/src/components/Login/Input.jsx
@@ -1,7 +1,7 @@
-const Input = ({ label, ...props }) => {
+const Input = ({ label, labelClassName = "", ...props }) => {
   return (
     <div className="flex flex-col">
-      <label className="mb-1 text-sm font-medium text-white">{label}</label>
+      <label className={`mb-1 text-sm font-medium ${labelClassName}`}>{label}</label>
       <input
         className="p-2 rounded-md text-black"
         {...props}
