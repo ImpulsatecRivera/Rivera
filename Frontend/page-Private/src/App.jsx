f4fdfdeb3df82e1afa9961a2760481e1a3dee031
Angel <angelfan1637@gmail.com>
2025-06-18 21:36:42 -0600

    Agregar pantalla de restablecer contraseña

--- Frontend/page-Private/src/App.jsx
+++ Frontend/page-Private/src/App.jsx
@@ -3,7 +3,8 @@ import { Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import RecoverPassword from "./pages/RecoverPassword";
 import VerificationCode from "./pages/VerificationCode";
-import VerificationInput from "./pages/VerificationInput"; 
+import VerificationInput from "./pages/VerificationInput";
+import ResetPassword from "./pages/ResetPassword";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Route path="/recuperar" element={<RecoverPassword />} />
       <Route path="/verification-code" element={<VerificationCode />} />
       <Route path="/verification-input" element={<VerificationInput />} />
+      <Route path="/reset-password" element={<ResetPassword />} />
     </Routes>
   );
 }
