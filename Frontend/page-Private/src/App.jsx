059a025f20f88bba0d04f4919130ae41b86ad57f
20230300@ricaldone.edu.sv <20230300@ricaldone.edu.sv>
2025-06-20 14:06:58 -0600

    VerClinente

--- Frontend/page-Private/src/App.jsx
+++ Frontend/page-Private/src/App.jsx
@@ -5,11 +5,12 @@ import RecoverPassword from "./pages/RecoverPassword";
 import VerificationCode from "./pages/VerificationCode";
 import VerificationInput from "./pages/VerificationInput";
 import ResetPassword from "./pages/ResetPassword";
+import ClientManagementInterface from "./pages/VerClientes"
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Login />} />
+      <Route path="/" element={<ClientManagementInterface />} />
       <Route path="/recuperar" element={<RecoverPassword />} />
       <Route path="/verification-code" element={<VerificationCode />} />
       <Route path="/verification-input" element={<VerificationInput />} />
