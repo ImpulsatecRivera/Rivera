fed8f82e22e2ba9a267c6a398b0826013b4235d2
Kendrick@gmail.com <Kendrick@gmail.com>
2025-06-20 11:06:05 -0600

    Recovery completado

--- Backend/src/Routes/Recovery.js
+++ Backend/src/Routes/Recovery.js
@@ -4,6 +4,8 @@ import RecoveryPass from "../Controllers/RecoveryController.js";
 const router=express.Router();
 
 router.route("/requestCode").post(RecoveryPass.requestCode);
+router.route("/verifyCode").post(RecoveryPass.verifyCode);
+router.route("/newPassword").post(RecoveryPass.newPassword);
 
 
 
