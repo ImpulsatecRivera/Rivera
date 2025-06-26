737c25513bf8049cfcb29a39cbeeb3e9cdc4226e
20230300@ricaldone.edu.sv <20230300@ricaldone.edu.sv>
2025-06-26 10:20:52 -0600

    agregar emp

--- Backend/src/Models/Empleados.js
+++ Backend/src/Models/Empleados.js
@@ -1,42 +1,44 @@
-import { Schema,model } from "mongoose";
+import { Schema, model } from "mongoose";
 
 const empleadoSchema = new Schema({
-    name:{
-        type:String,
+    name: {
+        type: String,
         required: true 
     },
-    lastName:{
-        type:String,
+    lastName: {
+        type: String,
         required: true 
     },
-    email:{
-        type:String,
+    email: {
+        type: String,
+        required: true,
+        unique: true // Asegurar que el email sea único
+    },
+    dui: { // Cambiar de 'id' a 'dui' para evitar conflictos
+        type: String,
+        required: true,
+        unique: true // Asegurar que el DUI sea único
+    },
+    birthDate: {
+        type: Date,
         required: true 
     },
-    id:{
-        type:String,
+    password: {
+        type: String,
         required: true 
     },
-    birthDate:{
-        type:Date,
-       required: true 
-    },
-    password:{
-        type:String,
-       required: true 
-    },
-    phone:{
-        type:String,
+    phone: {
+        type: String,
         required: true 
     },
-    address:{
-        type:String,
+    address: {
+        type: String,
         required: true 
     }
-},{
-    timestamps:true,
-    strict:false,
+}, {
+    timestamps: true,
+    strict: false,
     collection: "Empleados"
 });
 
-export default model ("Empleados",empleadoSchema);
+export default model("Empleados", empleadoSchema);
