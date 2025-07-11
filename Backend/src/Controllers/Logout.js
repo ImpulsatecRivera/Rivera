5dc4f08594dad37ffe11b00ad80b486241e3b589
Angel <angelfan1637@gmail.com>
2025-07-11 15:26:05 -0600

    Arreglando el login

--- Backend/src/Controllers/Logout.js
+++ Backend/src/Controllers/Logout.js
@@ -1,12 +1,12 @@
 const Logout = {};
 
 Logout.logout = async (req, res) => {
-    try {
-        res.clearCookie("authToken", { httpOnly: true });
-        return res.status(200).json({ Message: "Session cerrada" });
-    } catch (error) {
-        return res.status(500).json({ Message: "Error al eliminar el token" });
-    }
+  try {
+    res.clearCookie("authToken", { httpOnly: true });
+    return res.status(200).json({ Message: "Sesión cerrada" });
+  } catch (error) {
+    return res.status(500).json({ Message: "Error al cerrar sesión" });
+  }
 };
 
 export default Logout;
