9e9173f85e333c3b9e6396b76ff8fe1a7a970830
Angel <angelfan1637@gmail.com>
2025-07-11 15:26:05 -0600

    Arreglando el login

--- Backend/src/Routes/Logout.js
+++ Backend/src/Routes/Logout.js
@@ -1,9 +1,14 @@
-import express from "express"
-import Logout from "../Controllers/Logout.js"
+import express from "express";
 
 const router = express.Router();
 
-router.route("/")
-.post(Logout.logout);
+router.post("/", async (req, res) => {
+  try {
+    res.clearCookie("authToken", { httpOnly: true });
+    return res.status(200).json({ Message: "Sesión cerrada" });
+  } catch (error) {
+    return res.status(500).json({ Message: "Error al eliminar el token" });
+  }
+});
 
 export default router;
