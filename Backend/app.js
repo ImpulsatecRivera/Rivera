20cf5fd5282c642bfe623a748d31b4e175ca559d
20230300@ricaldone.edu.sv <20230300@ricaldone.edu.sv>
2025-07-25 10:28:09 -0600

    conflicttos areglados

--- Backend/app.js
+++ Backend/app.js
@@ -5,14 +5,14 @@ import motoristasRoutes from "./src/Routes/motoristaRoutes.js"
 import proveedoresRoutes from "./src/Routes/proveedoresRoutes.js"
 import ClientesRoutes from "./src/Routes/clienteRoutes.js"
 import LoginRoutes from "./src/Routes/LoginRoutes.js"
-<<<<<<< HEAD
+
 import LogoutRoutes from "./src/Routes/Logout.js" // ✅ Renombrado para claridad
 import RecoveryRoutes from "./src/Routes/Recovery.js"
-=======
+
 import LogoutRoutes from "./src/Routes/Logout.js" 
 import RecoveryRoutes from "./src/Routes/Recovery.js"
 import RegisterRoutes from "./src/Routes/RegisterRoute.js"
->>>>>>> 32c94ab92a55539026f958b94589d7d3bf77a044
+
 import cookieParser from "cookie-parser"
 import cors from "cors";
 
@@ -22,11 +22,8 @@ app.use(cookieParser());
 
 app.use(
   cors({
-<<<<<<< HEAD
     origin: "http://localhost:5173", 
-=======
     origin: ["http://localhost:5173","http://localhost:5174"], 
->>>>>>> 32c94ab92a55539026f958b94589d7d3bf77a044
     credentials: true, 
   })
 );
@@ -37,12 +34,9 @@ app.use("/api/motoristas", motoristasRoutes);
 app.use("/api/proveedores", proveedoresRoutes);
 app.use("/api/clientes", ClientesRoutes);
 app.use("/api/login", LoginRoutes);
-<<<<<<< HEAD
 app.use("/api/logout", LogoutRoutes); // ✅ Aquí corregido
-=======
 app.use("/api/register",RegisterRoutes);
 app.use("/api/logout", LogoutRoutes); 
->>>>>>> 32c94ab92a55539026f958b94589d7d3bf77a044
 app.use("/api/recovery", RecoveryRoutes);
 
 export default app;
