3304d0faf0eccbe0cda1e536192f95a666ae91d9
diego <20230602@ricaldone.edu.sv>
2025-07-31 14:49:25 -0600

    creacion carpeta movil

--- Backend/app.js
+++ Backend/app.js
@@ -5,37 +5,38 @@ import motoristasRoutes from "./src/Routes/motoristaRoutes.js"
 import proveedoresRoutes from "./src/Routes/proveedoresRoutes.js"
 import ClientesRoutes from "./src/Routes/clienteRoutes.js"
 import LoginRoutes from "./src/Routes/LoginRoutes.js"
-import LogoutRoutes from "./src/Routes/Logout.js" 
-import RecoveryRoutes from "./src/Routes/Recovery.js"
 
+ 
 import LogoutRoutes from "./src/Routes/Logout.js" // ✅ Renombrado para claridad
 import RecoveryRoutes from "./src/Routes/Recovery.js"
-
+ 
 import RegisterRoutes from "./src/Routes/RegisterRoute.js"
 import cookieParser from "cookie-parser"
 import cors from "cors";
-
+ 
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
-
+ 
 app.use(
   cors({
-    origin: "http://localhost:5173", 
-    origin: ["http://localhost:5173","http://localhost:5174"], 
-    credentials: true, 
+    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173","http://localhost:5174"],
+    credentials: true,
   })
 );
-
+ 
 app.use("/api/camiones", camionesRoutes);
 app.use("/api/empleados", empleadoRoutes);
 app.use("/api/motoristas", motoristasRoutes);
 app.use("/api/proveedores", proveedoresRoutes);
 app.use("/api/clientes", ClientesRoutes);
 app.use("/api/login", LoginRoutes);
-app.use("/api/logout", LogoutRoutes); 
+app.use("/api/logout", LogoutRoutes);
 app.use("/api/register",RegisterRoutes);
-
+ 
 app.use("/api/recovery", RecoveryRoutes);
-
+ 
 export default app;
+ 
+ 
