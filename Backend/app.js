5ac10d68eb06d7cceda45ac3937b052c30042277
Angel <angelfan1637@gmail.com>
2025-07-31 15:16:25 -0600

    resolviendo conflicto

--- Backend/app.js
+++ Backend/app.js
@@ -4,24 +4,13 @@ import empleadoRoutes from "./src/Routes/empleadosRoutes.js"
 import motoristasRoutes from "./src/Routes/motoristaRoutes.js"
 import proveedoresRoutes from "./src/Routes/proveedoresRoutes.js"
 import ClientesRoutes from "./src/Routes/clienteRoutes.js"
-<<<<<<< HEAD
-import LoginRoutes from "./src/Routes/LoginRoutes.js" 
-//servicio de auto-actualización
-import autoUpdateRoutes from './src/Routes/servicesRouter.js';
-=======
 import LoginRoutes from "./src/Routes/LoginRoutes.js"
-import LogoutRoutes from "./src/Routes/Logout.js" 
+import LogoutRoutes from "./src/Routes/Logout.js"
 import RecoveryRoutes from "./src/Routes/Recovery.js"
->>>>>>> master
-
-import LogoutRoutes from "./src/Routes/Logout.js" // ✅ Renombrado para claridad
-import RecoveryRoutes from "./src/Routes/Recovery.js"
-
 import RegisterRoutes from "./src/Routes/RegisterRoute.js"
-<<<<<<< HEAD
 import ViajesRoutes from "./src/Routes/ViajesRoutes.js"
-=======
->>>>>>> master
+import autoUpdateRoutes from './src/Routes/servicesRouter.js'
+
 import cookieParser from "cookie-parser"
 import cors from "cors";
 
@@ -31,8 +20,7 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: "http://localhost:5173", 
-    origin: ["http://localhost:5173","http://localhost:5174"], 
+    origin: ["http://localhost:5173", "http://localhost:5174"], 
     credentials: true, 
   })
 );
@@ -43,10 +31,10 @@ app.use("/api/motoristas", motoristasRoutes);
 app.use("/api/proveedores", proveedoresRoutes);
 app.use("/api/clientes", ClientesRoutes);
 app.use("/api/login", LoginRoutes);
-app.use("/api/logout", LogoutRoutes); 
-app.use("/api/register",RegisterRoutes);
-app.use('/api/auto-update', autoUpdateRoutes);
+app.use("/api/logout", LogoutRoutes);
+app.use("/api/register", RegisterRoutes);
 app.use("/api/recovery", RecoveryRoutes);
 app.use("/api/viajes", ViajesRoutes);
+app.use("/api/auto-update", autoUpdateRoutes);
 
 export default app;
