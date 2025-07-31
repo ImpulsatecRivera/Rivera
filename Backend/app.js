068097cd78e27dc69ffe940e293b02f4fc2f1a3d
kendricklopez1 <20200165@ricaldone.edu.sv>
2025-07-31 15:10:44 -0600

    Error solucionado

--- Backend/app.js
+++ Backend/app.js
@@ -4,28 +4,24 @@ import empleadoRoutes from "./src/Routes/empleadosRoutes.js"
 import motoristasRoutes from "./src/Routes/motoristaRoutes.js"
 import proveedoresRoutes from "./src/Routes/proveedoresRoutes.js"
 import ClientesRoutes from "./src/Routes/clienteRoutes.js"
-<<<<<<< HEAD
 import LoginRoutes from "./src/Routes/LoginRoutes.js" 
 //servicio de auto-actualización
 import autoUpdateRoutes from './src/Routes/servicesRouter.js';
-=======
 import LoginRoutes from "./src/Routes/LoginRoutes.js"
-<<<<<<< HEAD
-=======
+
 import LogoutRoutes from "./src/Routes/Logout.js" 
 import RecoveryRoutes from "./src/Routes/Recovery.js"
->>>>>>> master
->>>>>>> 4706c6c969a93576eb360082769273b31958e9d6
+
 
  
 import LogoutRoutes from "./src/Routes/Logout.js" // ✅ Renombrado para claridad
 import RecoveryRoutes from "./src/Routes/Recovery.js"
  
 import RegisterRoutes from "./src/Routes/RegisterRoute.js"
-<<<<<<< HEAD
+
 import ViajesRoutes from "./src/Routes/ViajesRoutes.js"
-=======
->>>>>>> master
+
+
 import cookieParser from "cookie-parser"
 import cors from "cors";
  
@@ -49,16 +45,15 @@ app.use("/api/clientes", ClientesRoutes);
 app.use("/api/login", LoginRoutes);
 app.use("/api/logout", LogoutRoutes);
 app.use("/api/register",RegisterRoutes);
-<<<<<<< HEAD
+
  
 app.use("/api/recovery", RecoveryRoutes);
  
-=======
+
 app.use('/api/auto-update', autoUpdateRoutes);
 app.use("/api/recovery", RecoveryRoutes);
 app.use("/api/viajes", ViajesRoutes);
 
->>>>>>> 4706c6c969a93576eb360082769273b31958e9d6
 export default app;
  
  
