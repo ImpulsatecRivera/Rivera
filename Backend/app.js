dd0f7356eaf08ea99419bbefeb9b11938c7e7c1d
20230300@ricaldone.edu.sv <20230300@ricaldone.edu.sv>
2025-08-20 17:02:39 -0600

    databse corregido

--- Backend/app.js
+++ Backend/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import ViajesRoutes from "./src/Routes/ViajesRoutes.js"
 
 const app = express();
 
@@ -20,5 +21,7 @@ app.get("/health", (req, res) => {
     timestamp: new Date().toISOString()
   });
 });
+
+app.use("/api/viajes", ViajesRoutes);
 
 export default app;
