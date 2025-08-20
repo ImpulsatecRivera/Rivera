5bca0f9bd085ba62af700600f9d4344cf32eafbd
20230300@ricaldone.edu.sv <20230300@ricaldone.edu.sv>
2025-08-20 16:57:17 -0600

    databse corregido

--- Backend/app.js
+++ Backend/app.js
@@ -1,10 +1,15 @@
 import express from "express";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors({
+  origin: ["https://rivera-project-ecru.vercel.app"],
+  credentials: true
+}));
 
 app.get("/test", (req, res) => {
   res.json({ message: "Test with cookieParser" });
