5ed4a7ce95fba021db52fb4015b631cb0e07684a
20230300@ricaldone.edu.sv <20230300@ricaldone.edu.sv>
2025-08-20 17:51:06 -0600

    headers app 6

--- Backend/app.js
+++ Backend/app.js
@@ -13,6 +13,7 @@ import camionesRoutes from "./src/Routes/camionesRoutes.js";
 import empleadoRoutes from "./src/Routes/empleadosRoutes.js";
 import motoristasRoutes from "./src/Routes/motoristaRoutes.js";
 import proveedoresRoutes from "./src/Routes/proveedoresRoutes.js";
+import autoUpdateRoutes from "./src/Routes/autoUpdateRoutes.js";
 
 const app = express();
 
@@ -59,5 +60,6 @@ app.use("/api/camiones", camionesRoutes);
 app.use("/api/empleados", empleadoRoutes);
 app.use("/api/motoristas", motoristasRoutes);
 app.use("/api/proveedores", proveedoresRoutes);
+app.use("/api/auto-update", autoUpdateRoutes);
 
 export default app;
