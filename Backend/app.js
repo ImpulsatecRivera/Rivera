1f8c689462f81da2d1038f5f28a6d728c3845d1c
20230300@ricaldone.edu.sv <20230300@ricaldone.edu.sv>
2025-08-21 10:18:24 -0600

    testeo8

--- Backend/app.js
+++ Backend/app.js
@@ -25,8 +25,8 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
   origin: [
-    "https://rivera-project-olive.vercel.app",
-    "https://rivera-project-k7xd.vercel.app"
+    "https://rivera-project-dun.vercel.app",
+    "https://rivera-project-gp76.vercel.app"
   ],
   credentials: true,
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
