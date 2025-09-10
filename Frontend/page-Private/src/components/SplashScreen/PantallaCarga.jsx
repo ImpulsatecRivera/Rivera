cb1c61c49f94f6883c7bde5d68936c98c8eda095
20230300@ricaldone.edu.sv <20230300@ricaldone.edu.sv>
2025-09-10 15:59:29 -0600

    cambio de animacion en pantalla de carga

--- Frontend/page-Private/src/components/SplashScreen/PantallaCarga.jsx
+++ Frontend/page-Private/src/components/SplashScreen/PantallaCarga.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import Lottie from 'lottie-react';
 // Importa tu animación JSON
-import carAnimation from '../../assets/lotties/Car _ Ignite Animation.json';
+import carAnimation from '../../assets/lotties/Delivery Truck _ Ignite Animation.json';
 
 const PantallaCarga = ({ onLoadingComplete }) => {
   const [animationComplete, setAnimationComplete] = useState(false);
