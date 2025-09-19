50ec59f8aee9c23bf995039d42c9890ea78e334f
20230300@ricaldone.edu.sv <20230300@ricaldone.edu.sv>
2025-09-18 20:05:25 -0600

    Path corregidos v2

--- Frontend/Page-Public/src/pages/Homee/Home.jsx
+++ Frontend/Page-Public/src/pages/Homee/Home.jsx
@@ -2,12 +2,12 @@ import React, { useState, useEffect } from 'react';
 import { Mail, Phone, MapPin, Facebook, Instagram, Linkedin, Twitter } from 'lucide-react';
 import './Home.css';
 import logo from '../../assets/image.png';
-import Equipo from "../../Images/Imagen1.jpg";
+import Equipo from "../../Images/Imagen2.jpg";
 import Nosotros from "../../Images/Imagen2.jpg";
-import Equipo3 from "../../Images/Imagen12.jpg";
+import Equipo3 from "../../Images/imagen12.jpg";
 import Equipo4 from "../../Images/imagen15.jpg";
 import Equipo5 from "../../Images/imagen16.jpg";
-import Equipo7 from "../../Images/Imagen18.jpg";
+import Equipo7 from "../../Images/imagen18.jpg";
 import Ceo from "../../Images/Imagen4.jpg";
 import Conoce from "../../Images/imagen14.jpg";
 
@@ -126,7 +126,7 @@ const Home = () => {
               nuestra empresa
             </h2>
             <p className="intro-text">
-              La empresa ofrecer servicios integral de transportes de carga, distribuciów y gestión logística a nivel nacional e internacional. Nos enfocamos en ser eficientes y puntuales, adaptándonos a las necesidades de cada cliente, con un equipo profesional y una ética de manufactura que garantizan calidad y confiabilidad en todo lo que hacemos.
+              La empresa samuel ofrecer servicios integral de transportes de carga, distribuciów y gestión logística a nivel nacional e internacional. Nos enfocamos en ser eficientes y puntuales, adaptándonos a las necesidades de cada cliente, con un equipo profesional y una ética de manufactura que garantizan calidad y confiabilidad en todo lo que hacemos.
             </p>
           </div>
           <div className="intro-image-section">
