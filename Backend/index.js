2a1045a98dd31ebc06f3cfcf0dcebf77eee74dc4
GabContreras <contrerasgab01@gmail.com>
2025-11-11 22:15:04 -0600

    prueba docker

--- Backend/index.js
+++ Backend/index.js
@@ -2,9 +2,12 @@ import app from "./app.js";
 import "./database.js";
 
 async function main() {
-    const port = 4000;
-    app.listen(port);
-    console.log("Servidor con vida " + port);
-  }
+  // Google Cloud Run asigna el puerto automáticamente
+  const port = process.env.PORT || 4000;
+  
+  app.listen(port, '0.0.0.0', () => {
+    console.log(`✅ Servidor corriendo en puerto ${port}`);
+  });
+}
 
 main();
