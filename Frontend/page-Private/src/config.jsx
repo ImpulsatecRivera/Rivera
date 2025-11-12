b9e7f1e5a8de534812b904a2e598186292f73e10
rdlfperez@gmail.com <rdlfperez@gmail.com>
2025-11-12 11:13:59 -0600

    frontend server

--- Frontend/page-Private/src/config.jsx
+++ Frontend/page-Private/src/config.jsx
@@ -1,7 +1,7 @@
 export const config = {
     //Creé este archivo para facilitar el cambio de url backend.
     api: {
-        API_URL: "https://rivera-test-629395560179.us-west1.run.app"
+        API_URL: "https://rivera-test-629395560179.us-west1.run.app/api"
         //Acá cambiar solamente lo que está antes de /api, NO dejar espacios al final.
         //la /api es necesaria
     },
