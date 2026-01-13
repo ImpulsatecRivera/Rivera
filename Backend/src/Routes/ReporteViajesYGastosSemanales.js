bc243ac49129dbeed3ae6cc8a26912e6c17f9da7
GabContreras <contrerasgab01@gmail.com>
2026-01-12 20:50:09 -0600

    borré código innecesario

--- Backend/src/Routes/ReporteViajesYGastosSemanales.js
+++ Backend/src/Routes/ReporteViajesYGastosSemanales.js
@@ -1,12 +1,9 @@
 import express from 'express';
-import ReporteController from '../Controllers/ReporteViajesYGastosSemanalesController.js';
+import ReporteViajesYGastosSemanalesController from '../Controllers/ReporteViajesYGastosSemanalesController.js';
 
 const router = express.Router();
 
-// POST /api/reporte/viajes-gastos/semanal -> Retorna JSON con datos
-router.post('/semanal', ReporteController.generarReporteSemanal);
-
 // POST /api/reporte/viajes-gastos/semanal/pdf -> Genera y descarga PDF
-router.post('/pdf', ReporteController.generarPDFSemanal);
+router.post('/pdf', ReporteViajesYGastosSemanalesController.generarPDFSemanal);
 
 export default router;
