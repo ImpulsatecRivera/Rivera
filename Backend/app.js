f82930c7da950670c35014600c0eda5220b2df0c
GabContreras <contrerasgab01@gmail.com>
2026-01-14 23:53:04 -0600

    ya furula

--- Backend/app.js
+++ Backend/app.js
@@ -61,7 +61,8 @@ app.use(
   cors({
     origin: [
       "https://verdant-sunshine-da6504.netlify.app",
-      "https://rivera-test-emflc9r2l-gabriel-contreras-projects-462b2f95.vercel.app",
+      "https://rivera-test.vercel.app",
+      "https://rivera-test-git-master-gabriel-contreras-projects-462b2f95.vercel.app",
       "http://localhost:5173",
       /^exp:\/\/.*$/,
       /^http:\/\/.*\.exp\.direct.*$/,
